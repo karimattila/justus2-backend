d6c836e961fa6a796a6559571778de1d396d1dec
ViciGallen <gallen.victor@gmail.com>
2018-06-18 09:33:06 +0300

    Removing more redundant stuff

--- src/app.ts
+++ src/app.ts
@@ -21,13 +21,6 @@ dotenv.config({ path: ".env.example" });
 
 // Controllers (route handlers)
 import * as homeController from "./controllers/home";
-import * as userController from "./controllers/user";
-import * as contactController from "./controllers/contact";
-
-
-// API keys and Passport configuration
-import * as passportConfig from "./config/passport";
-
 
 // Create Express server
 const app = express();
@@ -91,22 +84,6 @@ app.use(
  * Primary app routes.
  */
 app.get("/", homeController.index);
-app.get("/login", userController.getLogin);
-app.post("/login", userController.postLogin);
-app.get("/logout", userController.logout);
-app.get("/forgot", userController.getForgot);
-app.post("/forgot", userController.postForgot);
-app.get("/reset/:token", userController.getReset);
-app.post("/reset/:token", userController.postReset);
-app.get("/signup", userController.getSignup);
-app.post("/signup", userController.postSignup);
-app.get("/contact", contactController.getContact);
-app.post("/contact", contactController.postContact);
-app.get("/account", passportConfig.isAuthenticated, userController.getAccount);
-app.post("/account/profile", passportConfig.isAuthenticated, userController.postUpdateProfile);
-app.post("/account/password", passportConfig.isAuthenticated, userController.postUpdatePassword);
-app.post("/account/delete", passportConfig.isAuthenticated, userController.postDeleteAccount);
-
 
 
 
