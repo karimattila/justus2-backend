0d5ddaf2b718bc55e3068e396f6640755a95635a
ViciGallen <gallen.victor@gmail.com>
2018-07-02 15:07:35 +0300

    Node and Postgre integration working on apache inside vagrant, some commands have to be run inside vagrant before working tho. Will make ansible files for these commands

--- src/app.ts
+++ src/app.ts
@@ -12,64 +12,73 @@ import { SESSION_SECRET } from "./util/secrets";
 // Load environment variables from .env file, where API keys and passwords are configured
 dotenv.config({ path: ".env.example" });
 
+// Create express server
+const app = express();
+
 // Controllers (route handlers)
 import * as homeController from "./controllers/home";
 
-// Import fs and ini modules
-const fs = require("fs");
-const ini = require("ini");
+const session = require ("express-session");
+// Redis, use for later
+// const RedisStore = require("connect-redis")(session);
+const pg = require ("pg");
 
-// Read ini file for later use
-const dbConfig = ini.parse(fs.readFileSync("/etc/justus-backend.ini", "utf-8"));
+const Pool = require("pg-pool");
 
-// Use dbSettings.[name] to access wanted variable from the ini file. eg dbSettings.host
-const dbSettings = dbConfig.database;
 
-// Testing if ini file parsing/reading works
-console.log("\n" + dbSettings.host + "\nINI settings test");
 
-// Create Express server
-const app = express();
-
-// Connect to Postgres
-const session = require("express-session");
-
-// Connection details
-const connectionObject = {
-  host: dbSettings.host,
-  port: parseInt(dbSettings.port),
-  name: dbSettings.name,
-  user: dbSettings.user,
-  pass: dbSettings.pass
-};
-const pgSession = require("connect-pg-simple")(session);
-const pgStoreConfig = {
-  pgPromise: require("pg-promise")({ promiseLib: require("bluebird") })({
-                     connectionObject }),
-  };
-  app.set("port", process.env.port || 3000);
-  app.set("views", path.join(__dirname, "../views"));
-  app.set("view engine", "pug");
-  app.use(compression());
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(expressValidator());
-  app.use(flash());
-  app.use(lusca.xframe("SAMEORIGIN"));
-  app.use(lusca.xssProtection(true));
-  app.use((req, res, next) => {
-    res.locals.user = req.user;
-    next();
+// Create a pool once per process and reuse it
+const pgPool = new Pool ({
+  adapter: "connect-pg-simple",
+  database: "justus",
+  user: "appaccount",
+  password: "postgres",
+  host: "localhost",
+  port: 5432,
 });
 
-  app.use(session({
-    store: new pgSession(pgStoreConfig),
-    secret: SESSION_SECRET,
-    resave: true,
-    autoreconnect: true,
-    saveUninitialized: true,
-    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }
-  }));
+app.use(session({
+  store: new(require("connect-pg-pool")(session))({
+    pool: pgPool
+  }),
+  secret: SESSION_SECRET,
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+  resave: true,
+  autoreconnect: true,
+  saveUninitialized: true,
+
+}));
+
+// CONNECT TO PSQL INSIDE VAGRANT "psql -h 10.10.10.10 -U postgres -d justus"
+
+app.set("port", 3000);
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "pug");
+app.get("/", homeController.index);
+app.use(expressValidator);
+app.use(flash);
+app.use(lusca.xframe("SAMEORIGIN"));
+app.use(lusca.xssProtection);
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+
+
+
+
+// Import fs and ini modules
+// const fs = require("fs");
+// const ini = require("ini");
+
+// Read ini file for later use
+// const dbConfig = ini.parse(fs.readFileSync("/etc/justus-backend.ini", "utf-8"));
+
+// // Use dbSettings.[name] to access wanted variable from the ini file. eg dbSettings.host
+// const dbSettings = dbConfig.database;
 
 // Express configuration
 // app.set("port", process.env.port || 3000);
@@ -101,7 +110,6 @@ const pgStoreConfig = {
 /**
  * Primary app routes.
  */
-app.get("/", homeController.index);
 
 
 
